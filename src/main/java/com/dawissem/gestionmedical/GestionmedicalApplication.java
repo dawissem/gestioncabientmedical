14fe5aab17cffe1841a84cbc15ce91e5575cca43
dawissem <dawissem.wm@gmail.com>
2024-10-28 22:26:51 +0100

    first commit

---
+++ src/main/java/com/dawissem/gestionmedical/GestionmedicalApplication.java
@@ -0,0 +1,13 @@
+package com.dawissem.gestionmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionmedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionmedicalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dawissem/gestionmedical/GestionmedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawissem.gestionmedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionmedicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
